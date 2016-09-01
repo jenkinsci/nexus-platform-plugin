43f35d97cd12d265480a3d41e7f31708a3f9de80
Justin Young <jyoung@sonatype.com>
2016-09-01 16:08:30 -0700

    Added missing license to NxrmPublisherDescriptorTest

--- src/test/java/com/sonatype/nexus/ci/nxrm/NxrmPublisherDescriptorTest.groovy
+++ src/test/java/com/sonatype/nexus/ci/nxrm/NxrmPublisherDescriptorTest.groovy
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2016-present Sonatype, Inc. All rights reserved.
+ * Includes the third-party code listed at http://links.sonatype.com/products/clm/attributions.
+ * "Sonatype" is a trademark of Sonatype, Inc.
+ */
 package com.sonatype.nexus.ci.nxrm
 
 import com.sonatype.nexus.ci.config.GlobalNexusConfiguration
