63c185e013cd2ad6d1feb0c65a079ecd528b9baf
bigspotteddog <jcava@sonatype.com>
2018-05-30 15:19:53 -0700

    Add a style tag and inline the stylesheet.

--- src/main/resources/org/sonatype/nexus/ci/iq/PolicyEvaluationProjectAction/jobMain.groovy
+++ src/main/resources/org/sonatype/nexus/ci/iq/PolicyEvaluationProjectAction/jobMain.groovy
@@ -17,15 +17,14 @@ import org.sonatype.nexus.ci.iq.PolicyEvaluationHealthAction
 import org.sonatype.nexus.ci.iq.PolicyEvaluationProjectAction
 
 def t = namespace(lib.JenkinsTagLib)
-def l = namespace(lib.LayoutTagLib)
 
 def projectAction = (PolicyEvaluationProjectAction) it
 def action = projectAction.getJob().lastCompletedBuild.getAction(PolicyEvaluationHealthAction.class)
 
 if (action) {
-  l.css(src: "${rootURL}/plugin/nexus-jenkins-plugin/css/nexus.css")
   table(class: 'iq-job-main-table') {
     t.summary(icon: '/plugin/nexus-jenkins-plugin/images/48x48/nexus-iq.png') {
+      style(type: "text/css", ".nexus-jenkins-error { background: #faf3d1; border: 1px solid #eac9a9; -moz-border-radius: 3px; -webkit-border-radius: 3px; border-radius: px; padding: 0; -moz-box-shadow: 0 0 5px #ccc8b3; -webkit-box-shadow: 0 0 5px #ccc8b3; box-shadow: 0 0 5px #ccc8b3; margin: 15px; letter-spacing: normal; text-align: center }  .iq-job-main-table { margin-top: 1em; margin-left: 1em; }  .iq-job-main-table img { margin-right: 0 !important; }  .iq-chiclet { display:inline-block; width:25px; text-align:center; border-radius:5px; -moz-border-radius:5px; color:white; margin-right: 5px; }  .iq-chiclet.critical { background-color: #bc012f; }  .iq-chiclet.severe { background-color: #f4861d; }  .iq-chiclet.moderate { background-color: #f5c648; }")
       a(href: "lastCompletedBuild/${action.getUrlName()}", Messages.IqPolicyEvaluation_LatestReportName())
       br()
       img(src: "${rootURL}/plugin/nexus-jenkins-plugin/images/16x16/governance-badge.png")
